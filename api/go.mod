806cd76d09591b795d4e880fbad469da2c6d84e1
Noé Ksiazek <noe@ksiazek.xyz>
2024-10-22 20:50:38 +0200

    reorganize folder structure

---
+++ api/go.mod
@@ -0,0 +1,3 @@
+module github.com/yanek/hominous/api
+
+go 1.23.2
---
+++ api_legacy/src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { LinksController } from './links/links.controller';
+import { LinksService } from './links/links.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Link } from './links/link.entity';
+import { LinksModule } from './links/links.module';
+import { CategoriesModule } from './categories/categories.module';
+import { Category } from './categories/category.entity';
+import { CategoriesService } from './categories/categories.service';
+import { CategoriesController } from './categories/categories.controller';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: configService.get<string>('DATABASE_PATH'),
+          entities: [Link, Category],
+          synchronize: configService.get<string>('NODE_END') === 'development',
+        };
+      },
+    }),
+    LinksModule,
+    CategoriesModule,
+  ],
+  controllers: [AppController, LinksController, CategoriesController],
+  providers: [LinksService, CategoriesService],
+})
+export class AppModule {}
